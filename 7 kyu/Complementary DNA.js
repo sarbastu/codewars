9e87999d6f5b3368e5fcc1c4333bb46fba026ab2
Sarbast Umar <35075061+SarbastU@users.noreply.github.com>
2023-03-25 11:31:56 -0400

    Updated description

--- 7 kyu/Complementary DNA.js
+++ 7 kyu/Complementary DNA.js
@@ -1,4 +1,6 @@
 /*
+Description
+
 Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
 
 If you want to know more: http://en.wikipedia.org/wiki/DNA
