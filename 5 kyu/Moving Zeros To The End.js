e2e4ac4986bc4aa42b35d9a78058c2885a4ac856
Sarbast Umar <35075061+SarbastU@users.noreply.github.com>
2023-03-26 01:03:33 -0400

    "Moving Zeros To The End", "Cats and shelves"

---
+++ 5 kyu/Moving Zeros To The End.js
@@ -0,0 +1,20 @@
+/*
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+*/
+
+function moveZeros(arr) {
+    arr.reduceRight((acc, e, i) => {
+        if (e === 0) {
+        arr.splice(i, 1)
+        arr.push(0)
+        }
+    }, null)
+    return arr
+}
+
+// Best practice:
+// var moveZeros = function (arr) {
+//     return arr.filter(function(x) {return x !== 0}).concat(arr.filter(function(x) {return x === 0;}));
+// }
