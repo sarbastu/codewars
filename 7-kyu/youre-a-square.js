387586a73d842b9dd035c121420244c0332b6336
Sarbast Umar <35075061+SarbastU@users.noreply.github.com>
2023-04-29 00:30:05 -0400

    Completed You're a Square

---
+++ 7-kyu/youre-a-square.js
@@ -0,0 +1,18 @@
+/* Description:
+Given an integral number, determine if it's a square number:
+*/
+
+// My solution
+var isSquare = function (n) {
+  for (let i = 0; i <= n; i++) {
+    if (i * i === n) return true;
+  }
+  return false;
+};
+
+// Most voted solution
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
+
+const isSquare = (n) => Number.isInteger(Math.sqrt(n));
