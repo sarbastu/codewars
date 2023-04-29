2ff2fc491595ef742e0bd4fe4f2bdf6ded693013
Sarbast Umar <35075061+SarbastU@users.noreply.github.com>
2023-04-29 00:46:44 -0400

    Completed a few easy ones

---
+++ 8-kyu/convert-a-boolean-to-string.js
@@ -0,0 +1,13 @@
+/* DESCRIPTION:
+Implement a function which convert the given boolean value into its string representation.
+*/
+
+// My solution
+function booleanToString(b) {
+  return b ? "true" : "false";
+}
+
+// Most voted solution
+function booleanToString(b) {
+  return b.toString();
+}
---
+++ 8-kyu/you-cant-code-under-pressure-1.js
@@ -0,0 +1,13 @@
+/* DESCRIPTION:
+Code as fast as you can! You need to double the integer and return it.
+*/
+
+// My solution
+function doubleInteger(i) {
+  return i * 2;
+}
+
+// Other  solution
+function doubleInteger(i) {
+  return i + i;
+}
