4a9c398b7759dbb2fd413ec07de6aaebfea30b09
Sarbast Umar <35075061+SarbastU@users.noreply.github.com>
2023-05-01 01:05:44 -0400

    Completed Tribonacci Sequence

---
+++ 6-kyu/tribonacci-squence.js
@@ -0,0 +1,29 @@
+// DESCRIPTION:
+/* ==========================================================================
+As the name may already reveal, it works basically like a 
+Fibonacci, but summing the last 3 (instead of 2) numbers of the 
+sequence to generate the next.
+========================================================================== */
+/* My solution 
+================================================ */
+function tribonacci(signature, n) {
+  let sequence = []
+
+  for (let i = 0, m = 1; i < n; i++) {
+    sequence[i - 3] === undefined
+      ? sequence.push(signature[i])
+      : sequence.push(sequence[i - 1] + sequence[i - 2] + sequence[i - 3])
+  }
+
+  return sequence
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function tribonacci(signature, n) {
+  for (var i = 0; i < n - 3; i++) {
+    signature.push(signature[i] + signature[i + 1] + signature[i + 2])
+  }
+  return signature.slice(0, n)
+}
