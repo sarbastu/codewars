ac0d7f0ab8da2ac894af9d38633efa71167f6aa7
Sarbast Umar <35075061+SarbastU@users.noreply.github.com>
2023-05-08 23:21:24 -0400

    Completed Sum of two lowest positive integers

---
+++ 7-kyu/sum-of-two-lowest-positive-integers.js
@@ -0,0 +1,23 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Create a function that returns the sum of the two lowest positive numbers 
+given an array of minimum 4 positive integers. No floats or non-positive 
+integers will be passed.
+
+Example 
+sumTwoSmallestNumbers([19, 5, 42, 2, 77]) === 7
+========================================================================== */
+/* My solution 
+================================================ */
+function sumTwoSmallestNumbers(numbers) {
+  numbers = numbers.sort((a, b) => a - b)
+  return numbers[0] + numbers[1]
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function sumTwoSmallestNumbers(numbers) {
+  var [a, b] = numbers.sort((a, b) => a - b)
+  return a + b
+}
