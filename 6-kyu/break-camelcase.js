3c8234d458a779762ef0e9a7816189b8775932a8
sarbastu <sarbastu@gmail.com>
2024-09-13 05:50:52 -0400

    Completed: Breaking CamelCase

---
+++ 6-kyu/break-camelcase.js
@@ -0,0 +1,22 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Complete the solution so that the function will break up camel casing, using 
+a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+========================================================================== */
+/* My solution 
+================================================ */
+function solution(string) {
+  return string.split(/(?=[A-Z])/).join(' ')
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function solution(string) {
+  return string.replace(/([A-Z])/g, ' $1')
+}
