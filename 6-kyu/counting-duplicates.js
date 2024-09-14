5e75e33e49806c67e1414ed688719b2521835210
sarbastu <sarbastu@gmail.com>
2024-09-14 04:57:21 -0400

    Completed: Counting Duplicates

---
+++ 6-kyu/counting-duplicates.js
@@ -0,0 +1,41 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Write a function that will return the count of distinct case-insensitive 
+alphabetic characters and numeric digits that occur more than once in the 
+input string.
+
+Example
+"abcde" -> 0
+"aabbcde" -> 2
+"aA11" -> 2
+========================================================================== */
+/* My solution 
+================================================ */
+function duplicateCount(text) {
+  let letterCount = {}
+  let totalDupes = 0
+
+  text
+    .toLowerCase()
+    .split('')
+    .forEach((e) => {
+      letterCount[e] = letterCount[e] ? letterCount[e] + 1 : 1
+      letterCount[e] === 2 && totalDupes++
+    })
+
+  return totalDupes
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function duplicateCount(text) {
+  return (
+    text
+      .toLowerCase()
+      .split('')
+      .sort()
+      .join('')
+      .match(/([^])\1+/g) || []
+  ).length
+}
