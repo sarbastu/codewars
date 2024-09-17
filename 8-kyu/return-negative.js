8e8db3e0b2953ffc3eb7fab374985b9c7785c8d8
sarbastu <sarbastu@gmail.com>
2024-09-17 10:52:02 -0400

    Completed: Return Negative

---
+++ 8-kyu/return-negative.js
@@ -0,0 +1,21 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+In this simple assignment you are given a number and have to make it 
+negative.
+
+Examples
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+========================================================================== */
+/* My solution 
+================================================ */
+function makeNegative(num) {
+  return Number(`-${num}`) || num
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function makeNegative(num) {
+  return -Math.abs(num)
+}
