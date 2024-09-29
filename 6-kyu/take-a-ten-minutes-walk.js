259f8d17b642dcfd1c1edb0d06f2357e25fb43dc
sarbastu <sarbastu@gmail.com>
2024-09-28 22:43:02 -0400

    Completed: 6 Kyu - Take a Ten Minutes Walk

---
+++ 6-kyu/take-a-ten-minutes-walk.js
@@ -0,0 +1,54 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+You live in the city of Cartesia where all roads are laid out in a perfect 
+grid. You arrived ten minutes too early to an appointment, so you decided 
+to take the opportunity to go for a short walk. The city provides its 
+citizens with a Walk Generating App on their phones -- everytime you press 
+the button it sends you an array of one-letter strings representing 
+directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single 
+block for each letter (direction) and you know it takes you one minute to 
+traverse one city block, so create a function that will return true if the 
+walk the app gives you will take you exactly ten minutes (you don't want to 
+be early or late!) and will, of course, return you to your starting point. 
+Return false otherwise.
+========================================================================== */
+/* My solution 
+================================================ */
+function isValidWalk(walk) {
+  let walked = { n: 0, e: 0, s: 0, w: 0 }
+
+  if (walk.length !== 10) {
+    return false
+  } else {
+    walk.forEach((e) => walked[e]++)
+    return walked.n === walked.s && walked.e === walked.w
+  }
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function isValidWalk(walk) {
+  var dx = 0
+  var dy = 0
+  var dt = walk.length
+
+  for (var i = 0; i < walk.length; i++) {
+    switch (walk[i]) {
+      case 'n':
+        dy--
+        break
+      case 's':
+        dy++
+        break
+      case 'w':
+        dx--
+        break
+      case 'e':
+        dx++
+        break
+    }
+  }
+
+  return dt === 10 && dx === 0 && dy === 0
+}
