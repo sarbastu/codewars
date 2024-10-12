8270dfbf73eb4649414c61e2d62f1003993166df
sarbastu <sarbastu@gmail.com>
2024-10-12 01:23:32 -0400

    Completed: 8 Kyu - Grasshopper-Summation

---
+++ 8-kyu/grasshopper-summation.js
@@ -0,0 +1,29 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Summation
+Write a program that finds the summation of every number from 1 to num. The 
+number will always be a positive integer greater than 0. Your function only 
+needs to return the result, what is shown between parentheses in the example 
+below is how you reach that result and it's not part of it, see the sample 
+tests.
+
+For example (Input -> Output):
+2 -> 3 (1 + 2)
+8 -> 36 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8)
+========================================================================== */
+/* My solution 
+================================================ */
+var summation = function (num) {
+  let sum = 0
+  for (let i = 0; i < num; i++) {
+    sum = i + sum + 1
+  }
+  return sum
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function summation(num) {
+  return (num * (num + 1)) / 2
+}
