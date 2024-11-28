5fa9d85843e3501845adbd19519ca46fa91c00ab
sarbastu <sarbastu@gmail.com>
2024-11-28 14:22:48 -0500

    Completed 3 of JS

---
+++ 7_kyu/js_is_this_a_triangle.js
@@ -0,0 +1,32 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Implement a function that accepts 3 integer values a, b, c. The function 
+should return true if a triangle can be built with the sides of given length 
+and false in any other case.
+
+(In this case, all triangles must have surface greater than 0 to be 
+accepted).
+
+Examples:
+Input -> Output
+1,2,2 -> true
+4,2,3 -> true
+2,2,2 -> true
+1,2,3 -> false
+-5,1,3 -> false
+0,2,3 -> false
+1,2,9 -> false 
+========================================================================== */
+/* My solution 
+================================================ */
+function isTriangle(a, b, c) {
+  let [a1, a2, a3] = [a, b, c].sort((a, b) => a - b)
+  return a1 + a2 > a3
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function isTriangle(a, b, c) {
+  return a + b > c && a + c > b && c + b > a
+}
