b95e95452f957d594c5709bf37bedb54518342b1
sarbastu <sarbastu@gmail.com>
2025-01-09 12:39:43 -0500

    Add Your order, please

---
+++ 6_kyu/your_order_please.js
@@ -0,0 +1,40 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Your task is to sort a given string. Each word in the string will 
+contain a single number. This number is the position the word should 
+have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the 
+input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+========================================================================== */
+/* My solution 
+================================================ */
+function order(words) {
+  return words
+    .split(' ')
+    .sort((a, b) => +a.match(/\d/i)[0] - +b.match(/\d/)[0])
+    .join(' ');
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function order(words) {
+  var array = words.split(' ');
+  var sortedArray = [];
+  for (i = 0; i <= array.length; i++) {
+    for (j = 0; j < array.length; j++) {
+      if (array[j].indexOf(i) >= 0) {
+        sortedArray.push(array[j]);
+      }
+    }
+  }
+  return sortedArray.join(' ');
+}
