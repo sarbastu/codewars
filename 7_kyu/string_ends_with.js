5d2c0e5f4fe86a5939e92ed7c267c287e2d32717
sarbastu <sarbastu@gmail.com>
2025-01-17 17:00:08 -0500

    Add string ends with

---
+++ 7_kyu/string_ends_with.js
@@ -0,0 +1,26 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Complete the solution so that it returns true if the first argument(string) 
+passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false
+========================================================================== */
+/* My solution 
+================================================ */
+function solution(str, ending) {
+  return ending === str.slice(-ending.length) || !ending.length;
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function solution(str, ending) {
+  return str.endsWith(ending);
+}
+
+function solution(str, ending) {
+  return str.slice(0 - ending.length) === ending;
+}
