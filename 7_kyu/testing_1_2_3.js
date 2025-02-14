2732a60bb922c956566c7b4e88cd03f56a612c52
sarbastu <sarbastu@gmail.com>
2025-02-13 19:52:53 -0500

    Add testing 1-2-3

---
+++ 7_kyu/testing_1_2_3.js
@@ -0,0 +1,35 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Your team is writing a fancy new text editor and you've been tasked with 
+implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line 
+prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and 
+space in between.
+
+Examples: (Input --> Output)
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]
+========================================================================== */
+/* My solution 
+================================================ */
+var number = function (array) {
+  return array.map((e, i) => `${i + 1}: ${e}`);
+};
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+var number = function (arr) {
+  var newArr = [];
+  if (arr.length === 0) {
+    return [];
+  } else {
+    for (var i = 0, len = arr.length; i < len; i += 1) {
+      newArr.push(i + 1 + ': ' + arr[i]);
+    }
+  }
+  return newArr;
+};
