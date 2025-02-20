13a5d91388ecd26c82f37fd22dbfbd1c11015ea0
sarbastu <sarbastu@gmail.com>
2025-02-20 17:28:03 -0500

    Add alphabet war

---
+++ 7_kyu/alphabet_war.js
@@ -0,0 +1,60 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Introduction
+There is a war and nobody knows - the alphabet war!
+There are two groups of hostile letters. The tension between left side 
+letters and right side letters was too high and the war began.
+
+Task
+Write a function that accepts fight string consists of only small letters 
+and return who wins the fight. When the left side wins return Left side 
+wins!, when the right side wins return Right side wins!, in other case 
+return Let's fight again!.
+
+The left side letters and their power:
+ w - 4
+ p - 3
+ b - 2
+ s - 1
+
+The right side letters and their power:
+ m - 4
+ q - 3
+ d - 2
+ z - 1
+
+The other letters don't have power and are only victims.
+========================================================================== */
+/* My solution 
+================================================ */
+function alphabetWar(fight) {
+  const power = {
+    w: -4,
+    p: -3,
+    b: -2,
+    s: -1,
+    m: 4,
+    q: 3,
+    d: 2,
+    z: 1,
+  };
+
+  let score = fight.split('').reduce((a, e) => (a += power[e] || 0), 0);
+
+  if (score < 0) return 'Left side wins!';
+  if (score > 0) return 'Right side wins!';
+  return `Let's fight again!`;
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+alphabetWar = (fight) =>
+  ['Right side wins!', "Let's fight again!", 'Left side wins!'][
+    Math.sign(
+      [...fight].reduce(
+        (r, cv) => r + 'sbpw'.indexOf(cv) - 'zdqm'.indexOf(cv),
+        0
+      )
+    ) + 1
+  ];
