7191425569247ac43752f8957bab0c3351547133
sarbastu <sarbastu@gmail.com>
2025-03-03 18:06:34 -0500

    Add maximum length difference

---
+++ 7_kyu/maximum_length_difference.js
@@ -0,0 +1,44 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+You are given two arrays a1 and a2 of strings. Each string is composed 
+with letters from a to z. Let x be any string in the first array and y be 
+any string in the second array.
+
+Find max(abs(length(x) − length(y)))
+
+If a1 and/or a2 are empty return -1 in each language except in Haskell 
+(F#) where you will return Nothing (None).
+
+Example:
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", 
+      "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+mxdiflg(a1, a2) --> 13
+
+Bash note:
+- input : 2 strings with substrings separated by ,
+- output: number as a string
+========================================================================== */
+/* My solution 
+================================================ */
+function mxdiflg(a1, a2) {
+  if (!a1.length || !a2.length) return -1;
+
+  const arr1ItemLengths = a1.map((e) => e.length);
+  const arr2ItemLengths = a2.map((e) => e.length);
+
+  const firstMax = Math.max(...arr1ItemLengths) - Math.min(...arr2ItemLengths);
+  const secondMax = Math.max(...arr2ItemLengths) - Math.min(...arr1ItemLengths);
+
+  return Math.max(firstMax, secondMax);
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function mxdiflg(a1, a2) {
+  var max = -1;
+  for (var A1 of a1)
+    for (var A2 of a2) max = Math.max(max, Math.abs(A1.length - A2.length));
+  return max;
+}
