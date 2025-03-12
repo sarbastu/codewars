a0c5fc00cb8c138bce7ef8917e8b8fb4a128024b
sarbastu <sarbastu@gmail.com>
2025-03-12 17:30:19 -0400

    Add bumps in the road

---
+++ 7_kyu/bumps_in_the_road.js
@@ -0,0 +1,21 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Your car is old, it breaks easily. The shock absorbers are gone and you 
+think it can handle about 15 more bumps before it dies totally.
+
+Unfortunately for you, your drive is very bumpy! Given a string showing 
+either flat road (_) or bumps (n). If you are able to reach home safely by 
+encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+========================================================================== */
+/* My solution 
+================================================ */
+function bump(x) {
+  return x.match(/n/g)?.length > 15 ? 'Car Dead' : 'Woohoo!';
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function bump(str) {
+  return str.replace(/_/g, '').length > 15 ? 'Car Dead' : 'Woohoo!';
+}
