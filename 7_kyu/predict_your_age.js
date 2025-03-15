67b837792685df1b506ca001385e3b3d6553386c
sarbastu <sarbastu@gmail.com>
2025-03-14 20:01:30 -0400

    Add predict your age

---
+++ 7_kyu/predict_your_age.js
@@ -0,0 +1,31 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+My grandfather always predicted how old people would get, and right 
+before he passed away he revealed his secret!
+
+In honor of my grandfather's memory we will write a function using 
+his formula!
+
+Take a list of ages when each of your great-grandparent died.
+Multiply each number by itself.
+Add them all together.
+Take the square root of the result.
+Divide by two.
+Example
+predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+Note: the result should be rounded down to the nearest integer.
+
+Some random tests might fail due to a bug in the JavaScript 
+implementation. Simply resubmit if that happens to you.
+========================================================================== */
+/* My solution 
+================================================ */
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+  const sum = [...arguments].map((age) => age * age).reduce((a, b) => a + b);
+  const dividedSquareRoot = Math.sqrt(sum) / 2;
+  return Math.floor(dividedSquareRoot);
+}
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+const predictAge = (...ages) => (Math.hypot(...ages) / 2) | 0;
