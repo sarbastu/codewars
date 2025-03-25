45247119e900b4680f81802c3f337b3c66abcbc6
sarbastu <sarbastu@gmail.com>
2025-03-24 21:51:57 -0400

    Add Returning Strings

---
+++ 8_kyu/returning_strings.js
@@ -0,0 +1,17 @@
+/* DESCRIPTION: */
+/* ==========================================================================
+Create a function that accepts a parameter representing a name and returns 
+the message: "Hello, <name> how are you doing today?".
+========================================================================== */
+/* My solution 
+================================================ */
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+/* ===============================================
+* Solution by other users (usually highest voted) 
+================================================ */
+function greet(name) {
+  return 'Hello, ' + name + ' how are you doing today?';
+}
